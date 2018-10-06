4c98428ba5112cf547850cce137a4ae3efaa4572
mjanoudy <muaawiaj1991@hotmail.com>
2018-10-06 17:01:38 +0300

    finished bank and ship lab

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -20,7 +20,49 @@ class BankVault {
     
 }
 
+protocol ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool
+}
 
+extension BankVault: ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        if password.isEmpty {
+            return false
+        } else if password.count > 10 {
+            return false
+        } else {
+            var access = true
+            while access == true {
+                for i in stride(from: 0, to: password.count, by: 2) {
+                    if password[i] % 2 == 0 {
+                        access = true
+                    } else {
+                        access = false
+                    }
+                }
+            }
+            return access
+        }
+    }
+}
 
-
-
+//{
+   // var numCount = 0
+   // var trueCount = 0
+   // for i in stride(from: 0, to: password.count-1, by: 2) {
+    //    numCount += 1
+     //   let character = password[i]
+    //    switch character {
+    //    case 2, 4, 6, 8:
+      //      trueCount += 1
+       //     print("the \(i)th character is \(character)")
+       // default:
+      //      trueCount += 0
+     //   }
+ //   }
+    //if numCount == trueCount {
+   //     return true
+  //  } else {
+  //      return false
+  //  }
+//}
